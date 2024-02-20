4a313746bcab9bfe72e2059dfe5ac60c5d00e349
aponanwar <anwardu14@gmail.com>
2024-02-21 02:17:09 +0600

    smart ticket js done

--- scripts/mscripts.js
+++ scripts/mscripts.js
@@ -1,13 +1,78 @@
-const seats = document.getElementsByTagName('button');
-console.log(seats);
-let mySeatsName=[];
-for (let index = 3; index < 39; index++) {
-  const element = seats[index];
-//   console.log(element);
-  console.log(element.innerText);
-    mySeatsName.push(element.innerText);
+const allSeats = document.getElementsByClassName("seat");
+// console.log(allSeats);
+
+
+for (const seat of allSeats) {
+   seat.addEventListener("click", function (event) {
+     const seatName = event.target.innerText;
+     console.log(seatName);
+     const selectedContainer = document.getElementById("selected-seat-container");
     
+
+  
+     if (getValueById('cart') > 3) {
+       alert("tumar ticket boraddo sesh");
+       return;
+      }
+
+
+    //  event.target.setAttribute("disabled", false);
+    event.target.style.backgroundColor = "green";
+   
+     
+     const div = document.createElement('div');
+     div.classList.add('selected-seats');
+     div.classList.add('flex');
+     div.classList.add('justify-between');
+     div.classList.add('gap-1');
+     const p1 = document.createElement('p');
+     const p2 = document.createElement('p');
+     const p3 = document.createElement('p');
+     
+     p1.innerText = seatName;
+     p2.innerText = 'Economy';
+     p3.innerText = 550;
+
+     div.appendChild(p1);
+     div.appendChild(p2);
+     div.appendChild(p3);
+
+     
+     selectedContainer.appendChild(div);
+     const price = p3.innerText;
+     updateTotalCost(price);
+     updateSeatCount();
+  
+   
+   })
+  
+  
 }
 
 
 
+// useful functions that are called
+
+
+function updateTotalCost(price) {
+  const previousTotal = document.getElementById('total-cost').innerText;
+  const convertedTotal = parseInt(previousTotal);
+  const convertedPrice = parseInt(price);
+  const sum = convertedTotal + convertedPrice;
+  document.getElementById('total-cost').innerText = sum;
+  
+}
+
+
+function updateSeatCount() {
+  
+  const defaultSeatCount = document.getElementById('cart').innerText;
+  const convertedDefaultSeatCount = parseInt(defaultSeatCount);
+  document.getElementById('cart').innerText = convertedDefaultSeatCount + 1;
+  
+}
+
+function getValueById(id) {
+  const targetElement = document.getElementById(id).innerText;
+  return parseInt(targetElement);
+}
--- scripts/utility.js
+++
@@ -1,5 +0,0 @@
-// console.log('connected utility');
-function selection(element) {
-  const selected = document.getElementsByid(element)
-  selected.style.backgroundColor = 'red';
-}
